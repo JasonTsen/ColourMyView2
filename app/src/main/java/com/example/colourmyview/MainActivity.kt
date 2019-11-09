aca98480600acde08c5e7f8cbfa07bceca6a4b0e
Jason Tsen <sjtsen98@gmail.com>
2019-11-09 21:10:27 +0800

    coding challenge 1

--- app/src/main/java/com/example/colourmyview/MainActivity.kt
+++ app/src/main/java/com/example/colourmyview/MainActivity.kt
@@ -20,13 +20,21 @@ class MainActivity : AppCompatActivity() {
 
             // Boxes using Color for the background
 
+            R.id.box_one_text -> view.setBackgroundResource(R.drawable.img_one)
+            R.id.box_two_text -> view.setBackgroundResource(R.drawable.img_two)
+            R.id.box_three_text -> view.setBackgroundResource(R.drawable.img_three)
+            R.id.box_four_text -> view.setBackgroundResource(R.drawable.img_four)
+            R.id.box_five_text -> view.setBackgroundResource(R.drawable.img_five)
 
-            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
-            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
-            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
-            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
-            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
-
+//            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+//            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+//            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
+//            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
+//            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+            // Boxes using custom colors for background
+//            R.id.red_button -> box_three_text.setBackgroundResource(R.drawable.my_red)
+//            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+//            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
