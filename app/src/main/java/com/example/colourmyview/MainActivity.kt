08a5c4aea3e7c7507574d0742c9212b13722518a
Jason Tsen <sjtsen98@gmail.com>
2019-11-09 21:07:42 +0800

    coding challenge 2

--- app/src/main/java/com/example/colourmyview/MainActivity.kt
+++ app/src/main/java/com/example/colourmyview/MainActivity.kt
@@ -18,17 +18,15 @@ class MainActivity : AppCompatActivity() {
     private fun makeColored(view: View) {
         when (view.id) {
 
-            // Boxes using Color class colors for the background
-//            R.id.box_two_text -> view.setBackgroundResource(R.drawable.image_two)
+            // Boxes using Color for the background
+
+
             R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
             R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
             R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
-            // Boxes using custom colors for background
-            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
-            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
-            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
+
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
@@ -42,15 +40,12 @@ class MainActivity : AppCompatActivity() {
         val boxFiveText = findViewById<TextView>(R.id.box_five_text)
 
         val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
-        val redButton = findViewById<TextView>(R.id.red_button)
-        val greenButton = findViewById<TextView>(R.id.green_button)
-        val yellowButton = findViewById<TextView>(R.id.yellow_button)
+
 
             val clickableViews: List<View> =
                 listOf(
                     boxOneText, boxTwoText, boxThreeText,
-                    boxFourText, boxFiveText, rootConstraintLayout,
-                    redButton, greenButton, yellowButton
+                    boxFourText, boxFiveText, rootConstraintLayout
                 )
 
             for (item in clickableViews) {
